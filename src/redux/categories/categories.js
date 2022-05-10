b2a4038d1967d651c95e8d7d25d91d7d6bdecca4
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-05-10 13:46:49 +0430

    Add action and reducer for categories

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,13 @@
+export const categoriesCreator = {
+  type: "CHECKING_STATUS",
+};
+
+const categoriesReducer = (intialState = [], action) => {
+  switch (action.type) {
+    case "CHECKING_STATUS":
+      return "Under Construction";
+    default:
+      return intialState;
+  }
+};
+export default categoriesReducer();
