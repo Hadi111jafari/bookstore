eb881752212b2be294f05c8bc244d751ab4fe789
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-05-14 10:22:10 +0430

    Add provider

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import store from './redux/configureStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>  
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
-
-reportWebVitals();
